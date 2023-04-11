4e129d05ec941806c2eb0977d65004963d8a9737
Henzyd <henzydee@gmail.com>
2023-04-11 17:47:55 +0100

    Saving changes

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -14,4 +15,6 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
 });

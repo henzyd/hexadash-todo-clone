c6fa22aa34bb26b749a1d852835efc000f6d70c3
Henzyd <henzydee@gmail.com>
2023-04-12 22:33:29 +0100

    Saving changes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const globalErrorHandler = require("./controllers/errorController");
+const AppError = require("./utils/appError");
 
 const app = express();
 
@@ -8,10 +9,17 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+const baseRoute = "/v1";
+
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.status(200).json({ status: "success", message: "Welcome server" });
+app.get(`${baseRoute}/`, (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Welcome to the HexaDash-Todo server",
+  });
 });
+app.use(`${baseRoute}/auth`, require("./routes/authRoute"));
+app.use(`${baseRoute}/users`, require("./routes/userRoute"));
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
